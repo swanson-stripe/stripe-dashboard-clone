76dae7250fc82a06f3bd484d40f2f3907bce1bfa
swanson-stripe <swanson@stripe.com>
2025-05-16 15:50:33 -0700

    Remove chevron icon from metric trend indicators

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1066,15 +1066,6 @@ const Dashboard = () => {
                   <MetricValue>{metric.value}</MetricValue>
                   {metric.trendValue > 0 && (
                     <MetricTrend trend={metric.trend}>
-                      {metric.trend === 'up' ? (
-                        <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                          <path d="M18 15L12 9L6 15" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                        </svg>
-                      ) : (
-                        <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                          <path d="M6 9L12 15L18 9" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                        </svg>
-                      )}
                       {metric.trend === 'up' ? '+' : '-'}{metric.trendValue}%
                     </MetricTrend>
                   )}
